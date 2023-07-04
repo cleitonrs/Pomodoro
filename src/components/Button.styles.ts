61e32047d9f02352e857d330d36543841694b949
cleitonrs <rscleitonrs@gmail.com>
2023-07-04 14:46:29 -0300

    global styles

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -13,13 +13,20 @@ const buttonVariants = {
   success: 'green'
 }
 
+
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
 
-  ${props => {
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
+
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
-  }}
+  }} */
 `
