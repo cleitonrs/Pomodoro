0f852363203fe803d6bd011eb131864f038613c2
cleitonrs <rscleitonrs@gmail.com>
2023-07-05 10:23:08 -0300

    react-router-dom

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { History } from "./pages/HIstory"
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
