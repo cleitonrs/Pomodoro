aa1fa62d8df9385bf4d05932129f3fe225ab8a92
cleitonrs <rscleitonrs@gmail.com>
2023-07-10 21:37:11 -0300

    history component

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 3.5rem;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  margin-top: 2rem;
+  overflow: auto;
+
+  table {
+    width: 100%;
+    min-width: 600px;
+    border-collapse: collapse;
+  
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875;
+      line-height: 1.6rem;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  } 
+`
